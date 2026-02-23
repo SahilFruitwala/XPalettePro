263416ec11e501fdfc078287c82b0e8412181fdd
SahilFruitwala <sahilfruitwala007@gmail.com>
2026-02-22 22:04:37 -0500

    feat: Refine hover and focus styles for AppTabBar buttons and links

--- content.js
+++ content.js
@@ -357,8 +357,8 @@ div[data-testid="trend"]:hover,
 [role="menuitem"]:focus-visible,
 [role="option"]:hover,
 [role="option"]:focus-visible,
-[role="button"]:hover:not([data-testid$="-follow"]):not([data-testid$="-unfollow"]),
-[role="button"]:focus-visible:not([data-testid$="-follow"]):not([data-testid$="-unfollow"]) {
+[role="button"]:hover:not([data-testid$="-follow"]):not([data-testid$="-unfollow"]):not([data-testid^="AppTabBar_"]),
+[role="button"]:focus-visible:not([data-testid$="-follow"]):not([data-testid$="-unfollow"]):not([data-testid^="AppTabBar_"]) {
   background-color: var(--xp-interactive-hover) !important;
 }
 
@@ -371,13 +371,19 @@ div[data-testid="trend"]:hover,
 [data-testid^="AppTabBar_"][data-testid$="_Link"]:hover > div,
 [data-testid^="AppTabBar_"][data-testid$="_Link"]:focus-visible > div,
 [data-testid^="AppTabBar_"][data-testid$="_Link"]:hover > div > div:first-child,
-[data-testid^="AppTabBar_"][data-testid$="_Link"]:focus-visible > div > div:first-child {
+[data-testid^="AppTabBar_"][data-testid$="_Link"]:focus-visible > div > div:first-child,
+[data-testid="AppTabBar_More_Menu"]:hover > div,
+[data-testid="AppTabBar_More_Menu"]:focus-visible > div,
+[data-testid="AppTabBar_More_Menu"]:hover > div > div:first-child,
+[data-testid="AppTabBar_More_Menu"]:focus-visible > div > div:first-child {
   background-color: var(--xp-interactive-hover) !important;
   border-radius: 9999px !important;
 }
 
 [data-testid^="AppTabBar_"][data-testid$="_Link"] > div,
-[data-testid^="AppTabBar_"][data-testid$="_Link"] > div > div:first-child {
+[data-testid^="AppTabBar_"][data-testid$="_Link"] > div > div:first-child,
+[data-testid="AppTabBar_More_Menu"] > div,
+[data-testid="AppTabBar_More_Menu"] > div > div:first-child {
   border-radius: 9999px !important;
 }
 
